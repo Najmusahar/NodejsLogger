0fb90cf6a7725b9c333535935e25ba764fbdbdd3
najmusahar kalkoti <najmusaharkalkoti@gmail.com>
2024-09-20 16:10:47 +0530

    updated registerController with logger

--- Routes/registerRouter.js
+++ Routes/registerRouter.js
@@ -1,15 +1,15 @@
-import {  register, getAllUsers, updateUser, update, deleteUser, changePassword, createUser, login } from "../controller/registerController.js";
+import {  register, getAllUsers, update, deleteUser, changePassword, login } from "../controller/registerController.js";
 import express from "express";
 
 const registerRouter = express.Router();
 
 registerRouter.route("/register").post(register);
 registerRouter.route("/getUsers").get(getAllUsers);
-registerRouter.route("/updateUser/:id").post(updateUser);
+//registerRouter.route("/updateUser/:id").post(updateUser);
 registerRouter.route("/update/:userId").put(update);
 registerRouter.route("/delete/:userId").delete(deleteUser);
 registerRouter.route("/changePassword/:userId").post(changePassword);
-registerRouter.route("/createUser").post(createUser);
+//registerRouter.route("/createUser").post(createUser);
 registerRouter.route("/login").post(login);
 
 export default registerRouter;
--- controller/registerController.js
+++ controller/registerController.js
@@ -11,7 +11,7 @@ export const register = async(req,res)=>{
         const userDoc = await Register.findOne({email:email});
 
         if(userDoc){
-            console.log("Email already exists");
+            logger.error("Email already exists");
             return res.status(400).json({success:false,msg:"Email Id already exists"})
         }
 
@@ -21,12 +21,12 @@ export const register = async(req,res)=>{
             password,
             phone,
         });
-        console.log("User registered successfully");
+        logger.info("User registered successfully");
         return res.status(200).json({success:true,msg:"User registered successfully",newUser});
 
     }
     catch (error){
-        console.log(error);
+        logError(error);
         return res.status(500).json({success:false,msg:"Internal server error"});
     }
 }
@@ -34,35 +34,12 @@ export const register = async(req,res)=>{
 export const getAllUsers = async(req,res)=>{
     try{
         const users = await Register.find({});
+        logger.info(users);
         return res.status(200).json({success:true,users});
     }
     catch (error){
-        console.log(error);
+        logError(error);
         return res.status(500).json({success:false,users});
-    }
-}
-
-export const updateUser = async(req,res)=>{
-    try{
-        const userId = req.params.userId;
-        const {name,email,phone} = req.body;
-
-        const user = await Register.findOneAndUpdate({userId},{
-            name,
-            email,
-            phone,
-        });
-
-        if(!user){
-            console.log("user not found");
-            return res.status(404).json({success:false,msg:"user not found"});
-        }
-
-        return res.status(200).json({success:true,msg:"user updated successfully"});
-
-    }
-    catch (error){
-
     }
 }
 
@@ -90,10 +67,9 @@ export const update = async(req,res)=>{
     return res.status(200).json({success:true,msg:"user updated successfully"});
 }
 catch (error){
-    //logger.info(error);
     //console.log(error);
     logError(error);
-    return res.status(500).json({success:false,msg:"internal server error"});
+    return res.status(500).json({success:false,msg:"Internal server error"});
 }
 }
 
@@ -104,14 +80,16 @@ export const deleteUser = async(req, res)=>{
         const deleteDoc = await Register.findOneAndDelete({_id:userId});
 
         if(deleteDoc){
+            logger.info("Deleted Successfully");
             return res.status(200).json({success:true,msg:"Deleted successfully"});
         }
 
-        console.log("user not found");
+        logger.error("user not found");
             return res.status(404).json({success:false,msg:"user not found"});
     }
-    catch (error){
-
+    catch(error){
+        logError(error);
+        return res.status(500).json({success:false,msg:"Internal server error"});
     }
 }
 
@@ -119,49 +97,23 @@ export const changePassword = async(req,res)=>{
     try {
         const userId = req.params.userId;
         const {newPassword} = req.body;
-        console.log(userId,newPassword);
+
         const userDoc = await Register.findById(userId);
         if(!userDoc){
-            console.log("User id not valid "+userId);
+            logger.error("User id not valid ",userId);
             return res.status(400).json({success:false,msg:"user id not valid "+userId})
         }
         
         userDoc.password = newPassword;
     
         await userDoc.save();
-        console.log("success");
-        return res.status(200).json({success:true,msg:"Password updated successfully"})
+        logger.info("password changed successfully");
+        return res.status(200).json({success:true,msg:"Password updated successfully"});
 
     } 
     catch (error){
-        console.log(error);
+        logError(error);
         return res.status(500).json({sucess:false,msg:"internal server error"});
-    }
-}
-
-export const createUser = async(req, res)=>{
-    try{
-        const {name, email, phone, password, username}= req.body;
-        const user = await Register.findOne({email:email});
-
-        if(user){
-            console.log("Email id already exists");
-            return res.status(400).json({success:false,msg:"Email already exists"});
-        }
-
-        const newUser = await Register.create({
-            name,
-            email,
-            phone,
-            password,
-            username
-        })
-        console.log("user created successfully");
-        return res.status(200).json({success:true,msg:"user created successfully"});
-    }
-    catch(error){
-        console.log(error);
-        return res.status(500).json({success:false,msg:"internal server error"});
     }
 }
 
@@ -171,24 +123,24 @@ export const login = async(req, res)=>{
         const user = await Register.findOne({email:email});
         
         if(!user){
-            console.log("Email not found");
+            logger.error("Email not found");
+            //console.log("Email not found");
             return res.status(400).json({success:false,msg:"Email not found"});
         }
 
-        //const isMatch = await bcrypt.compare(password, user.password);
         if(user && (await user.matchPassword(password))){
+            logger.info("Login successfull");
             res.status(200).json({success:true,msg:"Login successfull",user});
         }
         else{
+            logger.info("Incorrect password");
             res.status(400).json({success:false,msg:"incorrect password"});
         }
-        // if(!isMatch){
-        //     console.log("Password not matched");
-        //     return res.status(400).json({success:false,msg:"Password not matched"});
-        //     }
+        
     }
     catch (error){
-        console.log(error);
+        logError(error);
+        //console.log(error);
         return res.status(500).json({success:false,msg:"internal server error"});
     }
 }
